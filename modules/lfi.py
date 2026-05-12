a9764f781f07ea3b9c9308756bc895ec71a9ef28
Mrx <143259711+akuajalah01@users.noreply.github.com>
2026-05-13 00:04:24 +0700

    Add LFIModule for local file inclusion detection

--- modules/lfi.py
+++ modules/lfi.py
@@ -1,1 +1,8 @@
-
+class LFIModule:
+    def __init__(self, target_url, proxy=None):
+        from core.scanner import Scanner
+        self.scanner = Scanner(target_url, proxy)
+    
+    def run(self, param):
+        is_vuln, _ = self.scanner.detect_lfi(param)
+        return {"vulnerable": is_vuln}
